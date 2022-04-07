cd0c8cd0ba43b16ff8768a21c9e07f508c1ee232
AyusmaTech <wojuolaayo@gmail.com>
2022-04-07 14:16:30 -0500

    Initial Commit

---
+++ Shared/BingWave_Sample_App_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BingWave_Sample_App_SwiftApp.swift
+//  Shared
+//
+//  Created by Ayusma on 07/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct BingWave_Sample_App_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
